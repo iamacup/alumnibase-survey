32160212b9738e4c40d22405b48bf842a3bf2e18
Megan Field <megan@sliips.com>
2018-05-02 10:52:29 +0100

    navigation changing sections

--- src/content/containers/Pages/NewTheme/navigation.js
+++ src/content/containers/Pages/NewTheme/navigation.js
@@ -6,7 +6,32 @@ import { connect } from 'react-redux';
 import { dataStoreIDSteps } from '../../../../content/containers/Pages/University/AllSteps';
 import { dNc } from '../../../../content/scripts/custom/utilities';
 
+import * as storeAction from '../../../../foundation/redux/globals/DataStoreSingle/actions';
+
+const dataStoreID = 'testHTML3';
+
 class Navigation extends React.PureComponent {
+  handleClick = (section) => {
+    const stepTo = section + '-1';
+
+    // let updateAnswerData = {};
+
+    //   if (!dNc(this.props.reduxState_this.answerData)) {
+    //   updateAnswerData = this.props.reduxState_this.answerData;
+    // }
+
+     this.props.reduxAction_doUpdate({
+        step: stepTo,
+    //     answerData: updateAnswerData,
+      });
+
+    const refObj = {
+      1: '8', 2: '11', 3: '4', 4: '7', 5: '5',
+    };
+
+    this.props.reduxAction_doUpdateStep({ currentStep: 1, stepCount: +refObj[section], section });
+  }
+
   render() {
     const { section } = this.props.reduxState_steps;
 
@@ -24,11 +49,11 @@ class Navigation extends React.PureComponent {
           </div>
         </div>
         <div className="list-group list-group-flush" data-toggle="collapse" aria-expanded="false" id="margin-sidebar">
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 1 ? ' button-active' : '')}>Section 1 - About You</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 2 ? ' button-active' : '')}>Section 2 - Uni Study</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 3 ? ' button-active' : '')}>Section 3 - Pre Uni</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 4 ? ' button-active' : '')}>Section 4 - Employment</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 5 ? ' button-active' : '')}>Section 5 - Retrospective</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 1 ? ' button-active' : '')} onClick={() => this.handleClick(1)}>Section 1 - About You</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 2 ? ' button-active' : '')} onClick={() => this.handleClick(2)}>Section 2 - Uni Study</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 3 ? ' button-active' : '')} onClick={() => this.handleClick(3)}>Section 3 - Pre Uni</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 4 ? ' button-active' : '')} onClick={() => this.handleClick(4)}>Section 4 - Employment</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 5 ? ' button-active' : '')} onClick={() => this.handleClick(5)}>Section 5 - Retrospective</button>
         </div>
         <div style={{ marginTop: '8px' }} />
 
@@ -37,9 +62,9 @@ class Navigation extends React.PureComponent {
         <div style={{ marginTop: '22px' }} />
 
         <div style={{ marginLeft: '38px' }} className="medium-grey-text">
-          <h6>some.email@example.com</h6>
-          <div style={{ marginTop: '16px' }} />
-          <span className="h6"><i className="fal fa-question-circle" style={{ fontSize: '16px' }} /> Help</span>
+          <a href="mailto:hello@alumnibaseapp.com?Subject=Hello"><h6 className="medium-grey-text">some.email@example.com</h6></a>
+          {/*   <div style={{ marginTop: '16px' }} />
+                 <span className="h6"><i className="fal fa-question-circle" style={{ fontSize: '16px' }} /> Help</span> */}
         </div>
       </div>
     );
@@ -48,6 +73,9 @@ class Navigation extends React.PureComponent {
 
 Navigation.propTypes = {
   reduxState_steps: PropTypes.object,
+  reduxState_this: PropTypes.object,
+  reduxAction_doUpdate: PropTypes.func,
+  reduxAction_doUpdateStep: PropTypes.func,
 };
 
 Navigation.defaultProps = {
@@ -56,12 +84,22 @@ Navigation.defaultProps = {
     stepCount: 7,
     section: 1,
   },
+  reduxState_this: {
+    step: '0-1',
+  },
+  reduxAction_doUpdate: () => {},
+  reduxAction_doUpdateStep: () => {},
 };
 
 const mapStateToProps = state => ({
   reduxState_steps: state.dataStoreSingle[dataStoreIDSteps],
+  reduxState_this: state.dataStoreSingle[dataStoreID],
 });
 
-const mapDispatchToProps = null;
+const mapDispatchToProps = dispatch => ({
+  reduxAction_doUpdate: data => dispatch(storeAction.doUpdate(dataStoreID, data)),
+  reduxAction_doUpdateStep: data => dispatch(storeAction.doUpdate(dataStoreIDSteps, data)),
+});
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
