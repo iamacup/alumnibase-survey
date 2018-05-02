4e42ee77c511392e14bd4cf4b0874d584960169f
Megan Field <megan@sliips.com>
2018-05-02 15:30:27 +0100

    navigation sidebar buttons working

--- src/content/containers/Pages/NewTheme/navigation.js
+++ src/content/containers/Pages/NewTheme/navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { dataStoreIDSteps } from '../../../../content/containers/Pages/University/AllSteps';
+import { dataStoreIDSteps, possibleSections } from '../../../../content/containers/Pages/University/AllSteps';
 import { dNc } from '../../../../content/scripts/custom/utilities';
 
 import * as storeAction from '../../../../foundation/redux/globals/DataStoreSingle/actions';
@@ -11,27 +11,6 @@ import * as storeAction from '../../../../foundation/redux/globals/DataStoreSing
 const dataStoreID = 'testHTML3';
 
 class Navigation extends React.PureComponent {
-  handleClick = (section) => {
-    const stepTo = section + '-1';
-
-    // let updateAnswerData = {};
-
-    //   if (!dNc(this.props.reduxState_this.answerData)) {
-    //   updateAnswerData = this.props.reduxState_this.answerData;
-    // }
-
-     this.props.reduxAction_doUpdate({
-        step: stepTo,
-    //     answerData: updateAnswerData,
-      });
-
-    const refObj = {
-      1: '8', 2: '11', 3: '4', 4: '7', 5: '5',
-    };
-
-    this.props.reduxAction_doUpdateStep({ currentStep: 1, stepCount: +refObj[section], section });
-  }
-
   render() {
     const { section } = this.props.reduxState_steps;
 
@@ -41,6 +20,59 @@ class Navigation extends React.PureComponent {
       theSection = section;
     }
 
+    console.log(this.props.reduxState_steps)
+
+
+
+    const handleClick = (sectionNum) => {
+
+      const realSection = this.props.reduxState_steps.realSection;
+      let realStep = this.props.reduxState_steps.realStep;
+      const stepName = this.props.reduxState_steps.stepName;
+
+      const summary = possibleSections[realSection][possibleSections[realSection].length-1];
+
+      //if the step we are moving to is 'not done' then we need to display not done
+      ///otherwise we move to the step and show the summary information
+      let stepTo = -1;
+      let sectionTo = -1;
+      let currentStep;
+
+// if sectionNum > realSection && realStep includes summary
+//  it should go to section-1
+console.log(possibleSections[sectionNum].length)
+      if(sectionNum > realSection) {
+        if (stepName === summary) {
+          stepTo = realSection + 1 + '-1';
+          sectionTo = sectionNum;
+          currentStep = possibleSections[realSection].indexOf(stepName);
+        } else {
+        //we know its not done
+          stepTo = 'not-done';
+          sectionTo = sectionNum;
+          currentStep = 1; 
+        }
+      } else if(sectionNum === realSection) {
+        //we need to do nothing
+        stepTo = possibleSections[sectionNum][realStep];
+        sectionTo = realSection;
+        currentStep = realStep + 1;
+      } else {
+        // we go to the section and show the 'summary' thing
+        stepTo = possibleSections[sectionNum][possibleSections[sectionNum].length-1];
+        sectionTo = sectionNum;
+        currentStep = possibleSections[sectionNum].length;
+      }
+
+      this.props.reduxAction_doUpdate({
+        step: stepTo,
+      });
+
+      this.props.reduxAction_doUpdateStep({ currentStep, stepCount:possibleSections[sectionNum].length , section: sectionTo });
+
+  }
+
+
     return (
       <div className="new-nav">
         <div className="d-flex justify-content-center" id="title-mobile">
@@ -49,11 +81,11 @@ class Navigation extends React.PureComponent {
           </div>
         </div>
         <div className="list-group list-group-flush" data-toggle="collapse" aria-expanded="false" id="margin-sidebar">
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 1 ? ' button-active' : '')} onClick={() => this.handleClick(1)}>Section 1 - About You</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 2 ? ' button-active' : '')} onClick={() => this.handleClick(2)}>Section 2 - Uni Study</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 3 ? ' button-active' : '')} onClick={() => this.handleClick(3)}>Section 3 - Pre Uni</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 4 ? ' button-active' : '')} onClick={() => this.handleClick(4)}>Section 4 - Employment</button>
-          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 5 ? ' button-active' : '')} onClick={() => this.handleClick(5)}>Section 5 - Retrospective</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 1 ? ' button-active' : '')} onClick={() => handleClick(1)}>Section 1 - About You</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 2 ? ' button-active' : '')} onClick={() => handleClick(2)}>Section 2 - Uni Study</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 3 ? ' button-active' : '')} onClick={() => handleClick(3)}>Section 3 - Pre Uni</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 4 ? ' button-active' : '')} onClick={() => handleClick(4)}>Section 4 - Employment</button>
+          <button type="button" className={'list-group-item list-group-item-action button' + (theSection === 5 ? ' button-active' : '')} onClick={() => handleClick(5)}>Section 5 - Retrospective</button>
         </div>
         <div style={{ marginTop: '8px' }} />
 
