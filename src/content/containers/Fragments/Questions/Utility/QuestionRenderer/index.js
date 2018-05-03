433f1a5b7abf0bbb1e7851df1fff41505d3925ff
Megan Field <megan@sliips.com>
2018-05-03 13:18:55 +0100

    company name to company to fix naming in question titles

--- src/content/containers/Fragments/Questions/Utility/QuestionRenderer/index.js
+++ src/content/containers/Fragments/Questions/Utility/QuestionRenderer/index.js
@@ -69,7 +69,6 @@ class QuestionRenderer extends React.Component {
   // todo this could be streamlined, horribly large ammount of code for two rexex replaces
   getNewTitle(title, answers) {
     // const results = [];
-
     // here we see if we need to replace any of the titles in the data object that are for {} replacements (non array)
     const regex1 = /\{([a-zA-Z]+\/[0-9]+_[0-9a-zA-Z]+)\.([0-9a-zA-Z]+)\|(.*?)\}/g;
     const regex2 = /\[([a-zA-Z]+\/[0-9]+)\.([0-9a-zA-Z]+)\|(.*?)\]/g;
@@ -88,19 +87,20 @@ class QuestionRenderer extends React.Component {
       // we now can see that we have a match and need to do something
       if (m.length === 4) {
         let questionID = m[1];
-        const friendlyName = m[2];
+        let friendlyName = m[2];
         const alternative = m[3];
-
         const arrayValue = questionID.split('_')[1];
         // eslint-disable-next-line prefer-destructuring
         questionID = questionID.split('_')[0];
 
+        if (friendlyName === "companyName") {
+          friendlyName = "company";
+        }
         // we pull out all the values that apply and then sort them so we can handle 'last', 'first' etc.
         // we do this by sorting it and then updating the questionID to be what we pull out as the final result
         // we loop the keys looking for something that starts with the questionID and has the friendlyName
         if (this.props.useMutatedTitles === true) {
           const foundItems = sortedRelevantBits(questionID, friendlyName, answers);
-
           if (arrayValue === 'latest' && foundItems.length > 0) {
             newTitle = title.replace(m[0], foundItems[foundItems.length - 1].data.optionValue);
           } else if (arrayValue === 'first' && foundItems.length > 0) {
@@ -112,7 +112,10 @@ class QuestionRenderer extends React.Component {
           newTitle = title.replace(m[0], alternative);
         }
       }
+// console.log(newTitle) // inside the while loop logs both titles with company and uni
     }
+// console.log(newTitle)  // outside only logs the title being passed to the question, newTitle is being replaced with the uni name?
+
 
     // THIS CHUNK LOOKS FOR ARRAY REPLACEMENTS
 
